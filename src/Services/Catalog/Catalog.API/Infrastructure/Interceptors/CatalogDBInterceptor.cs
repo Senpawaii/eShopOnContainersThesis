df8d2b57d1ee3e6c087d1ed0775b46b41fbdd6ad
Senpawaii <joao.miguel.queiros@gmail.com>
2023-07-04 12:32:47 +0100

    Performance Testing

--- src/Services/Catalog/Catalog.API/Infrastructure/Interceptors/CatalogDBInterceptor.cs
+++ src/Services/Catalog/Catalog.API/Infrastructure/Interceptors/CatalogDBInterceptor.cs
@@ -561,13 +561,21 @@ public class CatalogDBInterceptor : DbCommandInterceptor {
 
     [Trace]
     private void UpdateUpdateCommand(DbCommand command, string targetTable, string clientID) {
+        Stopwatch sw;
+
+        sw = Stopwatch.StartNew();
         // Get the timestamp received from the Coordinator
         string timestamp = _request_metadata.Timestamp.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
 
         // Replace Command Text to account for new parameter: timestamp
         string commandWithTimestamp;
         commandWithTimestamp = UpdateUpdateCommandText(command, targetTable);
-        
+
+        sw.Stop();
+        Console.WriteLine("Elapsed time 1: {0}", sw.Elapsed);
+        Timespans.Add(sw.Elapsed);
+        sw.Restart();
+
         // Add new Parameter and Command text to database command
         command.CommandText = commandWithTimestamp;
         // _logger.LogInformation($"ClientID: {clientID}, Updated command text: {command.CommandText}");
@@ -577,6 +585,11 @@ public class CatalogDBInterceptor : DbCommandInterceptor {
         clientTimestampParameter.Value = timestamp;
         command.Parameters.Add(clientTimestampParameter);
         // _logger.LogInformation($"Number of parameters: {command.Parameters.Count}");
+        sw.Stop();
+        Console.WriteLine("Elapsed time 2: {0}", sw.Elapsed);
+        Timespans2.Add(sw.Elapsed);
+        _logger.LogInformation($"Average time 1: {Average(Timespans)}");
+        _logger.LogInformation($"Average time 2: {Average(Timespans2)}");
     }
 
     [Trace]
