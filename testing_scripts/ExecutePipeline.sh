21d27aef90bfce60301f586d725522597548e49a
Senpawaii <joao.miguel.queiros@gmail.com>
2023-07-06 09:56:19 +0000

    Initial Commit for ExecutePipeline script.

---
+++ testing_scripts/ExecutePipeline.sh
@@ -0,0 +1,22 @@
+# Define the two versions of our system: MicroTCC and BaseTCC in a list
+systems=("µTCC", "BaseTCC")
+data_contention=("low", "high")
+test_VUs=(1, 10, 100, 1000)
+
+for i in $systems
+do
+    # Run the pipeline for each system
+    # 1. Disable/ Enable wrappers
+    if [ $i == "µTCC" ]; then
+        python3 Config_EnableDisableWrappers.py --thesisWrapper
+    else
+        python3 Config_EnableDisableWrappers.py
+    fi
+
+    # 2. Build the docker images
+    docker-compose build;
+
+done
+
+
+
