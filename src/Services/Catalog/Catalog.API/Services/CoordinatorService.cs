860b39f59a2d5ddec7b20256fe3a8e15f916a3a2
Senpawaii <joao.miguel.queiros@gmail.com>
2023-09-24 17:54:44 +0100

    [Catalog Service]: - Fixed token sending protocol to Coordinator

--- src/Services/Catalog/Catalog.API/Services/CoordinatorService.cs
+++ src/Services/Catalog/Catalog.API/Services/CoordinatorService.cs
@@ -23,7 +23,8 @@ public class CoordinatorService : ICoordinatorService {
     }
 
     public async Task SendTokens() {
-        int tokensToSend = _remainingTokens.GetRemainingTokens(_metadata.ClientID);
+        //int tokensToSend = _remainingTokens.GetRemainingTokens(_metadata.ClientID);
+        int tokensToSend = _metadata.Tokens;
         // _logger.LogInformation($"ClientID {_metadata.ClientID}: Send {tokensToSend} tokens to coordinator. Readonly={_metadata.ReadOnly}");
         string uri = $"{_settings.Value.CoordinatorUrl}tokens?tokens={tokensToSend}&clientID={_metadata.ClientID}&serviceName=CatalogService&readOnly={_metadata.ReadOnly}";
 
